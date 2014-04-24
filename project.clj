d177759ae75e410ab9456c492acc810158317f28
rossputin <rossajmcd@gmail.com>
2014-04-24 12:42:27 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Apache License v2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [clj-http "0.9.0"]
+                 [clj-http "0.9.1"]
                  [cheshire "5.3.1"]
-                 [com.taoensso/timbre "3.1.1"]]
+                 [com.taoensso/timbre "3.1.6"]]
   :aot :all
   :main protean.cli)
