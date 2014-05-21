1d32fa1921f6f8aefcedc34303768839e8111f87
Ross McDonald <Ross.McDonald@passivsystems.com>
2014-05-21 17:42:08 +0100

    upgrade references and text project(s) -> service(s)

--- src/protean/cli.clj
+++ src/protean/cli.clj
@@ -24,7 +24,7 @@
    ["-h" "--help"]])
 
 (defn usage [options-summary]
-  (->> ["Protean"
+  (->> ["Protean Command Line Interface"
         (get-version)
         ""
         "Usage: program-name [options] action"
@@ -33,14 +33,14 @@
         options-summary
         ""
         "Actions:"
-        "  projects               (List projects)"
-        "  project                -n myproject (List project)"
-        "  project-usage          -n myproject (List curl statements to use API)"
-        "  add-projects           -f project-config-file.edn (Add projects in a config file)"
-        "  del-project            -n myproject (Delete a project)"
-        "  add-project-error      -n myproject -s 500 (Add an error status code to a project)"
-        "  set-project-error-prob -n myproject -l 10 (Set error probability)"
-        "  del-project-errors     -n myproject (Delete error response codes)"
+        "  services               (List services)"
+        "  service                -n myservice (List service)"
+        "  service-usage          -n myservice (List curl statements to use API)"
+        "  add-services           -f service-config-file.edn (Add services in a codex)"
+        "  del-service            -n myservice (Delete a service)"
+        "  add-service-error      -n myservice -s 500 (Add an error status code to a service)"
+        "  set-service-error-prob -n myservice -l 10 (Set error probability)"
+        "  del-service-errors     -n myservice (Delete error response codes)"
         ""
         "Please refer to the manual page for more information."]
        (string/join \newline)))
@@ -52,42 +52,42 @@
 (defn exit [status msg] (println msg) (System/exit status))
 
 (defn projects [{:keys [host port]}]
-	(let [rsp (clt/get (str "http://" host ":" port "/projects"))]
+	(let [rsp (clt/get (str "http://" host ":" port "/services"))]
     (println (:body rsp))))
 
 (defn project [{:keys [host port name]}]
-  (let [rsp (clt/get (str "http://" host ":" port "/projects/" name))]
+  (let [rsp (clt/get (str "http://" host ":" port "/services/" name))]
     (println (:body rsp))))
 
 (defn project-usage [{:keys [host port name]}]
-  (let [rsp (clt/get (str "http://" host ":" port "/projects/" name "/usage"))]
+  (let [rsp (clt/get (str "http://" host ":" port "/services/" name "/usage"))]
     (doseq [j (jsn/parse-string (:body rsp))] (println j))))
 
 (defn add-projects [{:keys [file host port]}]
-  (let [rsp (clt/put (str "http://" host ":" port "/projects")
+  (let [rsp (clt/put (str "http://" host ":" port "/services")
               {:multipart [{:name "file"
                             :content (clojure.java.io/file file)}]})]
     (println (:body rsp))))
 
 (defn delete-project [{:keys [host port name] :as options}]
-  (let [rsp (clt/delete (str "http://" host ":" port "/projects/" name)
+  (let [rsp (clt/delete (str "http://" host ":" port "/services/" name)
                         {:throw-exceptions false})]
    (projects options)))
 
 (defn add-project-error [{:keys [host port name status-err] :as options}]
   (let [rsp
     (clt/put (str "http://" host ":" port
-                  "/projects/" name "/errors/status/" status-err))]
+                  "/services/" name "/errors/status/" status-err))]
     (project options)))
 
 (defn set-project-error-prob [{:keys [host port name level] :as options}]
   (let [rsp
     (clt/put (str "http://" host ":" port
-                  "/projects/" name "/errors/probability/" level))]
+                  "/services/" name "/errors/probability/" level))]
     (project options)))
 
 (defn del-project-errors [{:keys [host port name] :as options}]
-  (let [rsp (clt/delete (str "http://" host ":" port "/projects/" name "/errors"))]
+  (let [rsp (clt/delete (str "http://" host ":" port "/services/" name "/errors"))]
     (project options)))
 
 (defn -main [& args]
@@ -97,31 +97,31 @@
       (:help options) (exit 0 (usage summary))
       (not= (count arguments) 1) (exit 1 (usage summary))
       errors (exit 1 (error-msg errors))
-      (and (= (first arguments) "project")
+      (and (= (first arguments) "service")
            (not (:name options))) (exit 0 (usage summary))
-      (and (= (first arguments) "add-projects")
+      (and (= (first arguments) "add-services")
            (not (:file options))) (exit 0 (usage summary))
-      (and (= (first arguments) "del-project")
+      (and (= (first arguments) "del-service")
            (not (:name options))) (exit 0 (usage summary))
-      (and (= (first arguments) "add-project-error")
+      (and (= (first arguments) "add-service-error")
            (or (not (:name options))
                (not (:status-err options)))) (exit 0 (usage summary))
-      (and (= (first arguments) "set-project-error-prob")
+      (and (= (first arguments) "set-service-error-prob")
            (or (not (:name options))
                (not (:level options)))) (exit 0 (usage summary))
-      (and (= (first arguments) "del-project-errors")
+      (and (= (first arguments) "del-service-errors")
            (not (:name options))) (exit 0 (usage summary)))
     ;; Execute program with options
-    (println "Protean")
+    (println "Protean Command Line Interface")
     (println (get-version))
     (println "\n")
     (case (first arguments)
-      "projects" (projects options)
-      "project" (project options)
-      "project-usage" (project-usage options)
-      "add-projects" (add-projects options)
-      "del-project" (delete-project options)
-      "add-project-error" (add-project-error options)
-      "set-project-error-prob" (set-project-error-prob options)
-      "del-project-errors" (del-project-errors options)
+      "services" (projects options)
+      "service" (project options)
+      "service-usage" (project-usage options)
+      "add-services" (add-projects options)
+      "del-service" (delete-project options)
+      "add-service-error" (add-project-error options)
+      "set-service-error-prob" (set-project-error-prob options)
+      "del-service-errors" (del-project-errors options)
       (exit 1 (usage summary)))))
