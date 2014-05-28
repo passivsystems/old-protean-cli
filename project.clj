e1c155f29c961b1fd1b68abf2b0f8b03c5a9216b
Ross McDonald <Ross.McDonald@passivsystems.com>
2014-05-28 09:20:41 +0100

    bump version to 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protean-cli "0.4.0-pre.1"
+(defproject protean-cli "0.4.0"
   :description "Command line interface for Protean"
   :url "http://github.com/passivsystems/protean-cli"
   :license {:name "Apache License v2.0"
